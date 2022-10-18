3531521abc19a6c1b6c6ff586d369092b478f947
Or Geisler <orgeisler1@gmail.com>
2022-10-18 16:47:06 +0300

    querys 1-4

---
+++ querys.py
@@ -0,0 +1,81 @@
+import pymysql
+import json
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="poke_tracker",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+## query 1
+
+def heaviest_pokemon():
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT *  \
+                    FROM pokemon p \
+                    WHERE p.weight = (SELECT MAX(p.weight) FROM pokemon p)"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+
+    except:
+        print("Error")
+
+# print(heaviest_pokemon())
+
+## query 2
+
+def find_by_type(type):
+    try:
+        with connection.cursor() as cursor:
+            query = f"SELECT pokemon.name  \
+                    FROM pokemon JOIN type_of ON pokemon.id = type_of.pokemon_id \
+                    WHERE type_of.type_name = '{type}'"
+  
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+
+    except:
+        print("Error")
+
+# print(find_by_type('water'))
+
+## query 3
+
+def find_owners(poke_name):
+    try:
+        with connection.cursor() as cursor:
+            query = f"SELECT owned_by.trainer_name  \
+                    FROM pokemon JOIN owned_by ON pokemon.id = owned_by.pokemon_id \
+                    WHERE pokemon.name = '{poke_name}'"
+  
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+    except:
+        print("Error")
+
+print(find_owners('gengar'))
+
+## query 4
+
+def find_roster(trainer_name):
+    try:
+        with connection.cursor() as cursor:
+            query = f"SELECT pokemon.name  \
+                    FROM pokemon JOIN owned_by ON pokemon.id = owned_by.pokemon_id \
+                    WHERE owned_by.trainer_name = '{trainer_name}'"
+  
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+    except:
+        print("Error")
+
+print(find_roster('Loga'))
+
