179ad2d2e1754bca2cba21c45fef9682b57a0f80
Or Geisler <orgeisler1@gmail.com>
2022-10-18 15:24:28 +0300

    created tables

--- database_tables.sql
+++ database_tables.sql
@@ -3,24 +3,31 @@ USE poke_tracker;
 CREATE TABLE pokemon(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256),
-    type VARCHAR(256),
     height INT,
     weight INT
 );
 
 CREATE TABLE trainer(
-    name VARCHAR(256),
+    name VARCHAR(256) PRIMARY KEY,
     town VARCHAR(256)
-    PRIMARY KEY(name, town)
+);
+
+CREATE TABLE type(
+    name VARCHAR(256) PRIMARY KEY
 );
 
 CREATE TABLE owned_by(
     trainer_name VARCHAR(256),
-    trainer_town VARCHAR(256),
-    pokemon_name VARCHAR(256),
+    pokemon_id INT,
     FOREIGN KEY(trainer_name) REFERENCES trainer(name),
-    FOREIGN KEY(trainer_town) REFERENCES trainer(town),
-    FOREIGN KEY(pokemon_name) REFERENCES pokemon(name),
-    PRIMARY KEY(trainer_name, trainer_town,pokemon_name)
+    FOREIGN KEY(pokemon_id) REFERENCES pokemon(id),
+    PRIMARY KEY(trainer_name,pokemon_id)
+);
 
+CREATE TABLE type_of(
+    type_name VARCHAR(256),
+    pokemon_id INT,
+    FOREIGN KEY(type_name) REFERENCES type(name),
+    FOREIGN KEY(pokemon_id) REFERENCES pokemon(id),
+    PRIMARY KEY(type_name,pokemon_id)
 );
